06188ae4f02e9e1d122df50141cb61dfcafa220e
chitisan <jjchitisan@gmail.com>
2020-09-16 13:40:07 +0700

    aa1a

--- example.js
+++ example.js
@@ -1,1 +1,15 @@
-console.log("hello world")
+let name = "Anu"
+name = "jj"
+let test = "Hello"
+name = ${test} B
+let arr = ['apple','banana','kiwi']
+console.log("arr : ", arr[1])
+let person = {
+    name : 'jj',
+    age : 15,
+    gender :  {
+        name : 'female'
+    },
+    arr
+}
+console.log(person)
