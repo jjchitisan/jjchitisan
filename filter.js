27cf98f5b80cbad9021907d0bbc309be665095e4
chitisan <jjchitisan@gmail.com>
2020-09-16 14:50:00 +0700

    quiz

---
+++ filter.js
@@ -0,0 +1,2 @@
+let filferMoreThanZero = [-1,2,3,56,6,-93,0,4].filter(el => el >= 0)
+console.log(filferMoreThanZero)
---
+++ quiz2.js
@@ -0,0 +1,19 @@
+//1
+for(let i = 1; i<= 20 ; i+=1){
+    console.log(i)
+}
+
+//2
+data.forEach(e => e % 2 === 0 ? console.log(e) : '')
+//3
+data_1 = [-1, 2, 3, 56, 6, -93, 0, 4]
+const map_1 = data_1.map(e => e * 2)
+console.log(map_1)
+//4
+const data_2 = [10, 13, 44, 55, 89, 0, 12, 1]
+const result = []
+data_2.forEach(e => {
+    e % 2 === 0 ? result.push(e) : ''
+})
+console.log(result)
+
